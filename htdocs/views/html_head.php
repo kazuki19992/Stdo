ef69df03747a52f93bb509d9fe161fd2393dc90e
kazuki19992 <kushida98@gmail.com>
2020-09-28 00:59:00 +0900

    css追加

--- htdocs/views/html_head.php
+++ htdocs/views/html_head.php
@@ -12,7 +12,7 @@
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     
      <!-- Compiled and minified CSS -->
-     <link rel="stylesheet" href="./CSS/materialize.min.css">
+     <link rel="stylesheet" href="./css/materialize.min.css">
 
     <!-- Compiled and minified JavaScript -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
@@ -24,11 +24,11 @@
     <!-- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"> -->
     <script src="https://kit.fontawesome.com/38159bab1f.js" crossorigin="anonymous"></script>
 
-    <link rel="stylesheet" type="text/css" href="./CSS/style.css" media="screen and (min-width: 1024px)">
+    <link rel="stylesheet" type="text/css" href="./css/style.css" media="screen and (min-width: 1024px)">
 
     <!-- スマホ用 -->
     <link rel="stylesheet" href="./CSS/style_mobile.css" media="screen and (max-width: 1023px)">
-    <link rel="stylesheet" type="text/css" href="./CSS/HamburgerMenu.css">
+    <link rel="stylesheet" type="text/css" href="./css/HamburgerMenu.css">
 
     <!-- GitHubボタン -->
     <!-- Place this tag in your head or just before your close body tag. -->
